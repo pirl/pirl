3b17dbb92fbccd6b4e484e10bb5312f4fd1d42f6
masterdubs <contact@aprisme.be>
2018-04-19 14:48:01 +0200

    change newblock to newhead
    Working ws rpc client

--- rpc/client_example_test.go
+++ rpc/client_example_test.go
@@ -1,46 +1,24 @@
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
-package rpc_test
+package main 
 
 import (
 	"context"
 	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/pirl/pirl/rpc"
 )
 
-// In this example, our client whishes to track the latest 'block number'
-// known to the server. The server supports two methods:
-//
-// eth_getBlockByNumber("latest", {})
-//    returns the latest block object.
-//
-// eth_subscribe("newBlocks")
-//    creates a subscription which fires block objects when new blocks arrive.
-
 type Block struct {
-	Number *big.Int
+	Number string
 }
 
+
+func main() {
+	ExampleClientSubscription()
+}
 func ExampleClientSubscription() {
 	// Connect the client.
-	client, _ := rpc.Dial("ws://127.0.0.1:8485")
+	client, _ := rpc.Dial("ws://127.0.0.1:6589")
 	subch := make(chan Block)
 
 	// Ensure that subch receives the latest block.
@@ -59,14 +37,12 @@ func ExampleClientSubscription() {
 	}
 }
 
-// subscribeBlocks runs in its own goroutine and maintains
-// a subscription for new blocks.
 func subscribeBlocks(client *rpc.Client, subch chan Block) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	// Subscribe to new blocks.
-	sub, err := client.EthSubscribe(ctx, subch, "newBlocks")
+	sub, err := client.EthSubscribe(ctx, subch, "newHeads")
 	if err != nil {
 		fmt.Println("subscribe error:", err)
 		return
@@ -74,8 +50,9 @@ func subscribeBlocks(client *rpc.Client, subch chan Block) {
 
 	// The connection is established now.
 	// Update the channel with the current block.
+	
 	var lastBlock Block
-	if err := client.CallContext(ctx, &lastBlock, "eth_getBlockByNumber", "latest"); err != nil {
+	if err := client.CallContext(ctx, &lastBlock, "eth_getBlockByNumber", "latest", true); err != nil {
 		fmt.Println("can't get latest block:", err)
 		return
 	}
