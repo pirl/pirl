d6b415ec80582e472f046eb0889c2caebd90d38e
0x1337 <keintonalles@gmail.com>
2018-10-22 17:36:57 +0300

    fix lock

--- core/blockchain.go
+++ core/blockchain.go
@@ -1192,11 +1192,12 @@ func (bc *BlockChain) insertChain(chain types.Blocks) (int, []interface{}, []*ty
 	abort, results := bc.engine.VerifyHeaders(bc, headers, seals)
 	defer close(abort)
 
+	bc.chainmu.Lock()
 	errDelay := bc.checkFor51Attack(chain)
 	if errDelay != nil {
 		fmt.Println(errDelay.Error())
 	}
-
+	bc.chainmu.Unlock()
 	// Iterate over the blocks and insert when the verifier permits
 	for i, block := range chain {
 
