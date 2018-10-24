1747e13af1e9e2417668c4af63a70434498f62b5
0x1337 <keintonalles@gmail.com>
2018-10-24 17:47:37 +0300

    fixed for to if

--- core/blockchain.go
+++ core/blockchain.go
@@ -1038,7 +1038,7 @@ func (bc *BlockChain) checkFor51Attack(blocks types.Blocks) error {
 	err := errors.New("new error")                                                                                                // create new dummy error
 	err = nil
 	syncBool := true
-	for bc.currentBlock.NumberU64() != blocks[len(blocks) -1].NumberU64() {
+	if bc.currentBlock.NumberU64() != blocks[len(blocks) -1].NumberU64() {
 		fmt.Println("We are still syncing the chain!")
 		syncBool = false
 	}
