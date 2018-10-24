9c5610fdbf44d7ce00ceb83e28636ea6f45b102b
0x1337 <0x1337coding@gmail.com>
2018-10-24 15:37:51 +0300

    fix loop

--- core/blockchain.go
+++ core/blockchain.go
@@ -1067,7 +1067,7 @@ func (bc *BlockChain) checkFor51Attack(blocks types.Blocks) error {
 				for index = 0; index < params.HulkEnforcementBlockThreshold; index++ { // gather all incoming blocks with in the range
 					var ancestorToCheck *types.Block
 					for _, gb := range blocks {
-						if gb.NumberU64() == startIncomingBlock.NumberU64() {
+						if gb.NumberU64() == latestIncomingBlock - params.HulkEnforcementBlockThreshold {
 							ancestorToCheck = gb
 							fmt.Println("Ancestor to check in incoming chain :", ancestorToCheck.Header().Number.Uint64())
 						}
