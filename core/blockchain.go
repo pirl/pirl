d6923c38ba8a540436c65c0b696733f4947cd5a5
0x1337 <0x1337coding@gmail.com>
2018-10-26 14:55:22 +0300

    adding logs for new funcs

--- core/blockchain.go
+++ core/blockchain.go
@@ -1050,8 +1050,11 @@ func (bc *BlockChain) timeCapsule(blocks types.Blocks) error {
 	err := errors.New("new error")
 	err = nil
 	if blocks != nil && len(blocks) > 0 {
-		fmt.Println(len(blocks))
-		fmt.Println(bc.blockCache.Len())
+		fmt.Println("Blocks length :", len(blocks))
+		fmt.Println("Block cache length", bc.blockCache.Len())
+		fmt.Println("Body cache lenght", bc.bodyCache.Len())
+		fmt.Println("BC running", bc.running)
+
 		for _, v := range bc.blockCache.Keys() {
 			b, _ := GetBytes(v)
 			synced, syncErr := bc.db.Has(b)
