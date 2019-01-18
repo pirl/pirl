b4ead4e22d8e5fc8a8712d7498d4aa8a74047f92
0x1337 <keintonalles@gmail.com>
2019-01-18 13:06:47 +0200

    change to pirl

--- node/config.go
+++ node/config.go
@@ -239,7 +239,7 @@ func (c *Config) NodeName() string {
 	name := c.name()
 	// Backwards compatibility: previous versions used title-cased "Geth", keep that.
 	if name == "pirl" || name == "pirl-testnet" {
-		name = "Geth"
+		name = "Pirl"
 	}
 	if c.UserIdent != "" {
 		name += "/" + c.UserIdent
