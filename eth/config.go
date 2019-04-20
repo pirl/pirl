284da003157cc7dfbf015e9f8ca9e514c8334d78
Masterdubs <masterdubs@pirl.io>
2019-04-20 16:54:51 +0400

    netid

--- eth/config.go
+++ eth/config.go
@@ -43,7 +43,7 @@ var DefaultConfig = Config{
 		DatasetsInMem:  1,
 		DatasetsOnDisk: 2,
 	},
-	NetworkId:      1,
+	NetworkId:      3125659152,
 	LightPeers:     100,
 	DatabaseCache:  512,
 	TrieCleanCache: 256,
