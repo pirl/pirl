2432cb38c4603b1dc0f8a4349da39c83f2f4e7d8
0x1337 <keintonalles@gmail.com>
2019-04-22 16:16:10 +0300

    adding debug

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -640,6 +640,8 @@ func (ethash *Ethash) verifySeal(chain consensus.ChainReader, header *types.Head
 	}
 	// Verify the calculated values against the ones provided in the header
 	if !bytes.Equal(header.MixDigest[:], digest) {
+		fmt.Println(header.MixDigest[:])
+		fmt.Println(digest)
 		return errInvalidMixDigest
 	}
 	target := new(big.Int).Div(two256, header.Difficulty)
